1ff5d34c244a6c39b91fe3b90abb539233345428
Mur3ph <paulmurphymainstreet@gmail.com>
2018-10-09 15:55:47 +0100

    fixing null pointer exception

--- src/main/ie/murph/network/server/Provider.java
+++ src/main/ie/murph/network/server/Provider.java
@@ -6,9 +6,11 @@ import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
 
-public class Provider //implements Runnable
+public class Provider implements Runnable
 {
 	private Socket clientSocket;
+	private BufferedReader responseFromClient;
+	private PrintWriter requestFromClient;
 
 	public Provider(Socket socket)
 	{
@@ -29,23 +31,15 @@ public class Provider //implements Runnable
 		}
 		finally
 		{
-			try
-			{
-				System.out.println("Closing connection");
-				clientSocket.close();
-			}
-			catch (IOException e)
-			{
-				System.out.println("Unable to disconnect");
-				System.exit(1);
-			}
+			System.out.println("Closing connection");
+			closeConnection();
 		}
 	} // End of Thread inherited method run()..........
 
 	private void startRespondingToClient() throws IOException
 	{
-		BufferedReader responseFromClient = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-		PrintWriter requestFromClient = new PrintWriter(clientSocket.getOutputStream(), true);
+		responseFromClient = createInputStream();
+		requestFromClient = createOutputStream();
 
 		String input = responseFromClient.readLine();
 
@@ -62,6 +56,41 @@ public class Provider //implements Runnable
 				this.startRespondingToClient();
 			}
 		}
-		
+		System.out.println("You requested session to end.");
+		closeConnection();
 	}
+
+	private BufferedReader createInputStream() throws IOException
+	{
+		return new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+	}
+	
+	private PrintWriter createOutputStream() throws IOException
+	{
+		return new PrintWriter(clientSocket.getOutputStream(), true);
+	}
+	
+	private void closeConnection()
+	{
+		System.out.println("Closing connection.");
+
+		try
+		{
+			if (responseFromClient != null)
+				responseFromClient.close();
+
+			if (requestFromClient != null)
+				requestFromClient.close();
+
+			if (clientSocket != null)
+				clientSocket.close();
+
+			System.out.println("Closed...");
+		}
+		catch (IOException e)
+		{
+			System.out.println("Unable to disconnect..");
+			System.exit(1);
+		}
+	} // End of close connection method...
 }
