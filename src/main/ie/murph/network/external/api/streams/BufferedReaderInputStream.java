8b140e2a92ad297a068b13c209ace46ebb7ca837
Mur3ph <paulmurphymainstreet@gmail.com>
2018-12-21 22:57:29 +0000

    clean buffered reader

--- src/main/ie/murph/network/external/api/streams/BufferedReaderInputStream.java
+++ src/main/ie/murph/network/external/api/streams/BufferedReaderInputStream.java
@@ -1,10 +1,23 @@
 package main.ie.murph.network.external.api.streams;
 
 import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.Socket;
 
 public class BufferedReaderInputStream
 {
-	private BufferedReader bufinput = null;
-//	out = new PrintWriter(connection.getOutputStream(), true);
-//	bufinput = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+	private BufferedReader bufferedReader;
+	
+	public BufferedReaderInputStream(){}
+
+	public BufferedReader getBufinput()
+	{
+		return bufferedReader;
+	}
+
+	public void setBufferedReader(Socket socketConnection) throws IOException
+	{
+		this.bufferedReader = new BufferedReader(new InputStreamReader(socketConnection.getInputStream()));
+	}
 }
