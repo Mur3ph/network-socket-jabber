61c6421fef989d86765ab6f2face0d1aa18e30e4
Mur3ph <paulmurphymainstreet@gmail.com>
2019-01-01 23:19:54 +0000

    method name change

--- src/main/ie/murph/network/external/api/streams/BufferedReaderInputStream.java
+++ src/main/ie/murph/network/external/api/streams/BufferedReaderInputStream.java
@@ -11,7 +11,7 @@ public class BufferedReaderInputStream
 	
 	public BufferedReaderInputStream(){}
 
-	public BufferedReader getBufinput()
+	public BufferedReader getBufferedReader()
 	{
 		return bufferedReader;
 	}
