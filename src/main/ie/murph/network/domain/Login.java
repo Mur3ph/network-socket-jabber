523ff868685b957672ee4ccccb8ef3c1369b7253
Mur3ph <paulmurphymainstreet@gmail.com>
2019-01-15 23:14:52 +0000

    convering string to Character array

--- src/main/ie/murph/network/domain/Login.java
+++ src/main/ie/murph/network/domain/Login.java
@@ -7,75 +7,72 @@ import java.util.Map;
 import org.apache.log4j.LogManager;
 import org.apache.log4j.Logger;
 
-public class Login
-{
+public class Login {
 	private static final Logger LOGGER = LogManager.getLogger(Login.class.getName());
 	private String username;
 	private char[] password;
-	private Map<String, char[]> databaseOfUsers;
-	
-	public Login()
-	{
+	private Map<String, Character[]> databaseOfUsers;
+
+	public Login() {
 		System.getProperty("user.dir");
-		databaseOfUsers = new HashMap<String, char[]>();
+		databaseOfUsers = new HashMap<String, Character[]>();
 	}
-	
-	public String getUsername()
-	{
+
+	public String getUsername() {
 		return username;
 	}
-	public void setUsername(String username)
-	{
+
+	public void setUsername(String username) {
 		this.username = username;
 	}
-	public char[] getPassword()
-	{
+
+	public char[] getPassword() {
 		return password;
 	}
-	public void setPassword(char[] password)
-	{
+
+	public void setPassword(char[] password) {
 		this.password = password;
 	}
-	
-	public void resetPassword(char[] password)
-	{
+
+	public void resetPassword(char[] password) {
 		LOGGER.info("++resetPassword()");
 		this.password = password;
 	}
-	
-	public void addUserToDtabase(String usernameKey, char[] passwordValue)
-	{
+
+	public void addUserToDtabase(String usernameKey, Character[] passwordValue) {
 		LOGGER.info("++addUserToDtabase()");
 		databaseOfUsers.put(usernameKey, passwordValue);
 	}
-	
-	public char[] retrieveUserFromDtabase(String usernameKey)
-	{
+
+	public Character[] retrieveUserFromDtabase(String usernameKey) {
 		LOGGER.info("++retrieveUserFromDtabase()");
 		return databaseOfUsers.get(usernameKey);
 	}
-	
-	public boolean isUserExist(String usernameKey)
-	{
+
+	public boolean isUserExist(String usernameKey) {
 		LOGGER.info("++isUserExist()");
 		return databaseOfUsers.containsKey(usernameKey);
 	}
-	
-	public static void main(String[] args)
-	{
+
+	public static void main(String[] args) {
 		Login login = new Login();
 		String usernameKey = "Minnie";
-		char[] passwordValue = new char[]{'p','a','s','s','w','d'};
-		login.addUserToDtabase(usernameKey, passwordValue);
+		login.addUserToDtabase(usernameKey, convertStringToCharArrayJava8("passwd"));
 		String convertCharToString = convertCharToString(login, usernameKey);
-		LOGGER.info("Password: " + convertCharToString);
+		LOGGER.info("Password String: " + convertCharToString);
+		LOGGER.info("Password Character Array: " + convertStringToCharArrayJava8("passwd"));
+		LOGGER.info("Password Character Array: " + convertStringToCharArrayJava8("passwd"));
 	}
-	
+
 	private static String convertCharToString(Login login, String usernameKey) {
-		if(LOGGER.isDebugEnabled()){
-			LOGGER.debug("++convertCharToString()"); 
+		if (LOGGER.isDebugEnabled()) {
+			LOGGER.debug("++convertCharToString()");
 		}
 		return Arrays.toString(login.retrieveUserFromDtabase(usernameKey));// Convert char to string..;
+	}
+
+	private static Character[] convertStringToCharArrayJava8(String passwordValueStr) {
+		return passwordValueStr.chars().mapToObj(c -> (char) c).toArray(Character[]::new);
 	}
 
 }
