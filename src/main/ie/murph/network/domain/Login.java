ac2e6564c762cb8e879853cdc94c6a519e190f4a
Mur3ph <paulmurphymainstreet@gmail.com>
2019-02-07 21:51:24 +0000

    added hashmap to store list of messages for user

--- src/main/ie/murph/network/domain/Login.java
+++ src/main/ie/murph/network/domain/Login.java
@@ -1,7 +1,9 @@
 package main.ie.murph.network.domain;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import org.apache.log4j.LogManager;
@@ -12,7 +14,7 @@ public class Login {
 	private String username;
 	private char[] password;
 	private Map<String, Character[]> databaseOfUsers;
-	private Map<String, String> databaseOfUsersMessages;
+	private Map<String, List<String>> databaseOfUsersMessages;
 
 	public Login() {
 		System.getProperty("user.dir");
@@ -63,11 +65,63 @@ public class Login {
 	}
 
 	public Character[] convertStringToCharArrayJava8(String passwordValueStr) {
+		LOGGER.debug("++convertStringToCharArrayJava8()");
 		return passwordValueStr.chars().mapToObj(c -> (char) c).toArray(Character[]::new);
 	}
 
 	public char[] convertStringToChar(String password) {
+		LOGGER.debug("++convertStringToChar()");
 		return password.toCharArray();
 	}
+	
+	public Map<String, List<String>> getUsersMessages() {
+		LOGGER.debug("++getUsersMessages()");
+		return databaseOfUsersMessages;
+	}
+	
+	public void printUsersMessages() {
+		LOGGER.debug("++printUsersMessages()");
+		databaseOfUsersMessages.forEach((k,v)->System.out.println("Username: " + k + " Message: " + v));
+	}
 
+	public void sendUsersMessages(String username, String message) {
+		LOGGER.debug("++sendUsersMessages()");
+		List<String> list;
+		if(keyExists(username)){
+			LOGGER.debug("++sendUsersMessages()");
+			list = grabListOfMessagesBelongingTo(username);
+			addMessageToUsersList(list, message);
+		} else {
+			LOGGER.debug("++sendUsersMessages()");
+		    list = createNewList();
+		    addMessageToUsersList(list, message);
+		    createNewUserMessages(username, list);
+		}
+		LOGGER.debug("--sendUsersMessages()");
+	}
+
+	private boolean keyExists(String username2) {
+		LOGGER.debug("++keyExists()");
+		return databaseOfUsersMessages.containsKey(username);
+	}
+
+	private List<String> grabListOfMessagesBelongingTo(String username2) {
+		LOGGER.debug("++grabListOfMessagesBelongingTo()");
+		return databaseOfUsersMessages.get(username);
+	}
+	
+	private void addMessageToUsersList(List<String> list, String message) {
+		LOGGER.debug("++addMessageToUsersList()");
+		list.add(message);
+	}
+	
+	private List<String> createNewList() {
+		LOGGER.debug("++createNewList()");
+		return new ArrayList<String>();
+	}
+	
+	private void createNewUserMessages(String username, List<String> list) {
+		LOGGER.debug("++createNewUserMessages()");
+		databaseOfUsersMessages.put(username, list);
+	}
 }
