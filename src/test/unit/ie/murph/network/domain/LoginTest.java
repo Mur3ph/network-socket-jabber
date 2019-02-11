ddac78fe8b88b03b4a19ea88993ffc43f120e056
Mur3ph <paulmurphymainstreet@gmail.com>
2019-02-11 22:26:40 +0000

    clean login tests

--- src/test/unit/ie/murph/network/domain/LoginTest.java
+++ src/test/unit/ie/murph/network/domain/LoginTest.java
@@ -9,9 +9,9 @@ import main.ie.murph.network.domain.Login;
 
 public class LoginTest {
 	private static final Logger LOGGER = LogManager.getLogger(Login.class.getName());
-	private static final Login login = new Login();
-	private static final String testUsernameKey = "Minnie";
-	private static final String testPassword = "password";
+	private static final Login LOGIN = new Login();
+	private static final String TEST_USERNAME_MAP_KEY = "Minnie";
+	private static final String TEST_PASSWORD_MAP_VALUE = "password";
 
 	public static void main(String[] args) {
 		registerUserToDatabaseTest();
@@ -19,34 +19,34 @@ public class LoginTest {
 	}
 
 	private static void registerUserToDatabaseTest() {
-		login.registerNewUserToDtabase(testUsernameKey, testPassword);
+		LOGIN.registerNewUserToDtabase(TEST_USERNAME_MAP_KEY, TEST_PASSWORD_MAP_VALUE);
 		String convertCharToString = convertCharToString();
 		LOGGER.info("Password String: " + convertCharToString);
 		LOGGER.info("Password Character Array 1: " + convertStringToCharArrayJava8());
 		LOGGER.info("Password Character Array 2: " + Arrays.toString(convertStringToCharArrayJava8()));
 		LOGGER.info("Password char Array 1: " + convertStringToChar());
-		LOGGER.info("Correct Password: Should pass test: " + login.validateCredentialsUserFromDtabase(testUsernameKey, testPassword));
-		LOGGER.info("Incorrect Password: Should fail test: " + login.validateCredentialsUserFromDtabase(testUsernameKey, "wrong_password"));
+		LOGGER.info("Correct Password: Should pass test: " + LOGIN.validateCredentialsUserFromDtabase(TEST_USERNAME_MAP_KEY, TEST_PASSWORD_MAP_VALUE));
+		LOGGER.info("Incorrect Password: Should fail test: " + LOGIN.validateCredentialsUserFromDtabase(TEST_USERNAME_MAP_KEY, "wrong_password"));
 	}
 
 	private static String convertCharToString() {
 		if (LOGGER.isDebugEnabled()) {
 			LOGGER.debug("++convertCharToString()");
 		}
-		return Arrays.toString(login.retrieveUserFromDtabase(testUsernameKey));// Convert char to string..;
+		return Arrays.toString(LOGIN.retrieveUserFromDtabase(TEST_USERNAME_MAP_KEY));// Convert char to string..;
 	}
 
 	private static Character[] convertStringToCharArrayJava8() {
-		return testPassword.chars().mapToObj(c -> (char) c).toArray(Character[]::new);
+		return TEST_PASSWORD_MAP_VALUE.chars().mapToObj(c -> (char) c).toArray(Character[]::new);
 	}
 
 	private static char[] convertStringToChar() {
-		return testPassword.toCharArray();
+		return TEST_PASSWORD_MAP_VALUE.toCharArray();
 	}
 	
 	private static void registerUserWithMessagingTest() {
 		LOGGER.debug("++tesNewUserWithMessaging()");
-		login.registerNewUserToDtabase(testUsernameKey, testPassword);
+		LOGIN.registerNewUserToDtabase(TEST_USERNAME_MAP_KEY, TEST_PASSWORD_MAP_VALUE);
 		
 		populateUserMessages();
 		
@@ -56,17 +56,17 @@ public class LoginTest {
 
 	private static void populateUserMessages() {
 		for(int x = 0; x < 5; x++) {
-			login.sendUsersMessages(testUsernameKey, testPassword);
+			LOGIN.sendUsersMessages(TEST_USERNAME_MAP_KEY, TEST_PASSWORD_MAP_VALUE);
 		}
 	}
 
 	private static void printMappedUserWithMessages() {
 		LOGGER.debug("++tesNewUserWithMessaging() - MAP: ");
-		login.printMapOfUsersMessages(testUsernameKey);
+		LOGIN.printMapOfUsersMessages(TEST_USERNAME_MAP_KEY);
 	}
 	
 	private static void printListOfUserMesages() {
 		LOGGER.debug("++tesNewUserWithMessaging() - LIST: ");
-		login.printListOfUsersMessages(testUsernameKey);
+		LOGIN.printListOfUsersMessages(TEST_USERNAME_MAP_KEY);
 	}
 }
