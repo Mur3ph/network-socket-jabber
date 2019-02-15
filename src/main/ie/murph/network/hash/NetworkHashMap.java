6f0be1ba1fb331d26d8cfe58a63afb33c8e4c4b3
Mur3ph <paulmurphymainstreet@gmail.com>
2019-02-15 22:17:43 +0000

    create own hashmap

---
+++ src/main/ie/murph/network/hash/NetworkHashMap.java
@@ -0,0 +1,13 @@
+package main.ie.murph.network.hash;
+
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
+
+public class NetworkHashMap {
+	private static final Logger LOGGER = LogManager.getLogger(NetworkHashMap.class.getName());
+	public int hash = 31;
+
+	public static void main() {
+		LOGGER.info("++main()");
+	}
+}
