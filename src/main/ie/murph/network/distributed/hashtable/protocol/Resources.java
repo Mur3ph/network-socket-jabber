4b99de4658c1b2bab0afb6a4e7ab60418e2dae01
Mur3ph <paulmurphymainstreet@gmail.com>
2019-07-08 23:36:40 +0100

    ..more resources

--- src/main/ie/murph/network/distributed/hashtable/protocol/Resources.java
+++ src/main/ie/murph/network/distributed/hashtable/protocol/Resources.java
@@ -3,7 +3,8 @@ package main.ie.murph.network.distributed.hashtable.protocol;
 public enum Resources {
 	
 	CHORD_PEER_TO_PEER("https://en.m.wikipedia.org/wiki/Chord_(peer-to-peer)"),
-	CHORD_PEER_TO_PEER_IMAGE("https://www.google.com/search?q=Chord+(peer-to-peer)&client=firefox-b-d&channel=crow&source=lnms&tbm=isch&sa=X&ved=0ahUKEwiXmZn0g6DjAhXhUBUIHesvCpMQ_AUIECgB&biw=1920&bih=966");
+	CHORD_PEER_TO_PEER_IMAGE("https://www.google.com/search?q=Chord+(peer-to-peer)&client=firefox-b-d&channel=crow&source=lnms&tbm=isch&sa=X&ved=0ahUKEwiXmZn0g6DjAhXhUBUIHesvCpMQ_AUIECgB&biw=1920&bih=966"),
+	COLLEGE_LECTURE_2016("https://amplab.github.io/cs262a-fall2016/notes/19-Chord-Dynamo.pdf");
 	
 	private final String text;
 
--- src/main/ie/murph/network/distributed/hashtable/protocol/RunProtocol.java
+++ src/main/ie/murph/network/distributed/hashtable/protocol/RunProtocol.java
@@ -5,6 +5,7 @@ public class RunProtocol {
 	public static void main(String[] args){
 		System.out.println(Resources.CHORD_PEER_TO_PEER);
 		System.out.println(Resources.CHORD_PEER_TO_PEER_IMAGE);
+		System.out.println();
 	}
 	
 }
