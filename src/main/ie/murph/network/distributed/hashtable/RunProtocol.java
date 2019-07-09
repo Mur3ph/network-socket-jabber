ca59b8d9edd0347fd7aaf5155b651a11600eed9a
Mur3ph <paulmurphymainstreet@gmail.com>
2019-07-09 23:46:23 +0100

    creating objects for game to begin

--- src/main/ie/murph/network/distributed/hashtable/RunProtocol.java
+++ src/main/ie/murph/network/distributed/hashtable/RunProtocol.java
@@ -1,5 +1,6 @@
 package main.ie.murph.network.distributed.hashtable;
 
+import java.util.HashSet;
 import java.util.Hashtable;
 import java.util.Set;
 import java.util.UUID;
@@ -9,31 +10,24 @@ import main.ie.murph.network.distributed.hashtable.domain.User;
 
 public class RunProtocol {
 
-	private static final int SET_SIZE = 10;
-	private static Hashtable<User, Point> table;
+	private static final int SET_SIZE = 9;
+	private static Set<Point> points;
+	private static Hashtable<User, Set<Point>> table;
 	
 	public RunProtocol(){
-		table = new Hashtable<User, Point>();
+		points = new HashSet<>(SET_SIZE);
+		table = new Hashtable<User, Set<Point>>();
 	}
 
 	public static void main(String[] args){
-		learningREsources();
+//		learningREsources();
 		
 	}
-	private static void learningREsources(){
+	
+	public static void learningREsources(){
 		System.out.println(Resources.CHORD_PEER_TO_PEER);
 		System.out.println(Resources.CHORD_PEER_TO_PEER_IMAGE);
 		System.out.println();
-	}
-	
-	public static void run()
-	{
-		Set<String> javaSet = new java.util.HashSet<>(SET_SIZE);
-        for (int i = 0; i < SET_SIZE; i++) {
-            String randomHexString = UUID.randomUUID().toString().replaceAll("-", "");
-            javaSet.add(randomHexString);
-        }
-         
 	}
 	
 }
