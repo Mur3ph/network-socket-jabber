d46a35d592e4ad3c1a055bb9f37038759ce547a3
Mur3ph <paulmurphymainstreet@gmail.com>
2019-07-19 23:05:14 +0100

    map for history of games

--- src/main/ie/murph/network/distributed/hashtable/domain/History.java
+++ src/main/ie/murph/network/distributed/hashtable/domain/History.java
@@ -1,7 +1,10 @@
 package main.ie.murph.network.distributed.hashtable.domain;
 
+import java.util.Map;
+
 public class History {
 	private int gamesWon;
 	private int gamesLost;
+	private Map<String, Integer> games;
 	
 }
