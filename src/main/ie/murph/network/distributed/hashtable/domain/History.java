1f3d03eb127694a6778976e56630c26f062b354b
Mur3ph <paulmurphymainstreet@gmail.com>
2019-07-24 23:20:57 +0100

    hashmap innit

--- src/main/ie/murph/network/distributed/hashtable/domain/History.java
+++ src/main/ie/murph/network/distributed/hashtable/domain/History.java
@@ -1,10 +1,15 @@
 package main.ie.murph.network.distributed.hashtable.domain;
 
+import java.util.HashMap;
 import java.util.Map;
 
 public class History {
 	private int gamesWon;
 	private int gamesLost;
 	private Map<String, Integer> games;
+	
+	public History(){
+		games = new HashMap<String, Integer>();
+	}
 	
 }
