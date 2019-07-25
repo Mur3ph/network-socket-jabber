b18a22c44858b3cae2d684f8b43af4c506b34fbb
Mur3ph <paulmurphymainstreet@gmail.com>
2019-07-25 22:40:06 +0100

    ending game dev. clean code, fresh start

--- src/main/ie/murph/network/distributed/hashtable/RunProtocol.java
+++ src/main/ie/murph/network/distributed/hashtable/RunProtocol.java
@@ -1,36 +1,12 @@
 package main.ie.murph.network.distributed.hashtable;
 
-import java.util.HashSet;
-import java.util.Hashtable;
-import java.util.Map;
-import java.util.Set;
-
-import main.ie.murph.network.distributed.hashtable.domain.History;
-import main.ie.murph.network.distributed.hashtable.domain.Point;
-import main.ie.murph.network.distributed.hashtable.domain.User;
-
 public class RunProtocol {
 
-	private static final int SET_SIZE = 9;
-	private static Set<Point> points;
-//	For Map below the String will be date and History will be Users history :)
-	private static Hashtable<User, Map<String, History>> table;
-	
 	public RunProtocol(){
-		points = new HashSet<>(SET_SIZE);
-		table = new Hashtable<User, Map<String, History>>();
 	}
 
 	public static void main(String[] args){
 		
-	}
-	
-	public Hashtable<User, Map<String, History>> getHashTable(){
-		return table;
-	}
-	
-	public Set<Point> getPoints(){
-		return points;
 	}
 	
 	public static void learningResources(){
