9c9ce5d68c2a2c9560c5b7da5473678eb32e4f03
Mur3ph <paulmurphymainstreet@gmail.com>
2019-07-26 23:46:17 +0100

    adding resources

--- src/test/security/ie/murph/network/domain/RunSecurityTest.java
+++ src/test/security/ie/murph/network/domain/RunSecurityTest.java
@@ -8,10 +8,10 @@ public class RunSecurityTest {
 	public static void main(String[] args) {
 		LOGGER.info("Tools: https://dzone.com/articles/security-testing-tools-you-need-to-know-about");
 		LOGGER.info("Testing a security permission: https://blog.joda.org/2004/11/testing-security-permission_5894.html");
+		LOGGER.info("20 Tools for Ethical Hacking: https://www.guru99.com/learn-everything-about-ethical-hacking-tools-and-skills.html");
 		LOGGER.info("Security Testing");
 		LOGGER.info("Wireshark");
 		LOGGER.info("Nmap");
-		LOGGER.info("Cain & Able");
 		LOGGER.info("Cain & Able");
 	}
 
